ed5a9622709af0e9ab7c74600a5edbbef18ee975
wilderoliveira92 <wilder.oliveira92@gmail.com>
2020-02-25 20:35:46 -0300

    app github

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import './config/RetotronConfg';
+
+import Routes from './routes';
+
+export default function App() {
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <Routes />
+        </>
+    );
+}
---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+    createStackNavigator(
+        {
+            Main,
+            User,
+        },
+        {
+            defaultNavigationOptions: {
+                headerTitleAlign: 'center',
+                headerBackTitleVisible: false,
+                headerStyle: {
+                    backgroundColor: '#7159c1',
+                },
+                headerTintColor: '#FFF',
+            },
+        }
+    )
+);
+
+export default Routes;
+
+// stack - em plilha geralmente por
